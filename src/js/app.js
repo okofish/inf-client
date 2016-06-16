28438c3e4a8537891b6343f0b8e53eb76ac0f302
Jesse Friedman <jesse@jesse.ws>
2016-06-15 23:52:10 -0400

    Update 2016-06-16T03:52:09.449Z

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).foundation();
-
-$(document).ready(function() {
-  showStart();
-  
-  // always hacker-friendly :-)
-  console.log('Hello, adventurous console explorer!');
-  console.log('Try starting the game, then check out the properties of the `game` object.');
-});
